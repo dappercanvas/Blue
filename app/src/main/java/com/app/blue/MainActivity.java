698430cd4ccddc4c5353e637871cf9f41b5b5af7
nderitu_njau <nderitunjau@gmail.com>
2021-01-04 11:08:38 +0300

    Initial commit

---
+++ app/src/main/java/com/app/blue/MainActivity.java
@@ -0,0 +1,29 @@
+package com.app.blue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+    private static int splash_time_out=5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent next=new Intent(MainActivity.this,Home.class);
+                startActivity(next);
+                finish();
+            }
+        },splash_time_out);
+
+    }
+}
